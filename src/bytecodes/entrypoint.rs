79c4e463af9630c37f35e743366144f1d973f53e
SylStarz <jonnard.lucas@gmail.com>
2026-08-26 15:18:04 +0200

    v0.1.5

--- src/bytecodes/entrypoint.rs
+++ src/bytecodes/entrypoint.rs
@@ -2,7 +2,7 @@
 
 /// Lifecycle entrypoint opcodes for an Apica application execution.
 #[repr(u64)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, IntoPrimitive, TryFromPrimitive)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, IntoPrimitive, TryFromPrimitive)]
 pub enum ApicaEntrypointBytecode {
     /// Initialization hook (runs once on startup).
     Init =              0x00000000,
